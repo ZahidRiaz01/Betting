096cf8687956c6fbbec232cb8caf559fdc64c46e
ArbitechSolution <arbitechsolutions@gmail.com>
2022-05-20 05:59:58 -0700

    final
    Attached the api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+
 import "./App.css";
 import CronJob from "./cronJob/cronJob";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <div className="App">
+    <div className="App d-flex align-items-center">
+      <ToastContainer/>
       <CronJob />
     </div>
   );
--- frontend/src/cronJob/cronJob.js
+++ frontend/src/cronJob/cronJob.js
@@ -1,29 +1,92 @@
-import { Button } from "bootstrap";
+
+import React, { useRef, useState } from "react";
+import { Button, Toast } from "bootstrap";
 import { Dropdown } from "bootstrap";
-import React from "react";
+
 import "./cronJob.css";
-import { Form } from "react-bootstrap";
+import axios from 'axios'
+import { toast } from 'react-toastify';
 
 export default function CronJob() {
+
+let userEnteredAmount = useRef()
+let [selectedMethod, setSelectedMethod] = useState("betBull");
+let [ selectedBool, setSelectedBool] = useState(false);
+const getMethod=(e)=>{
+  console.log("You have selected",e.target.value);
+  setSelectedMethod(e.target.value)
+}
+const getBool =(e)=>{
+  console.log(" Your bool value is ", e.target.value);
+  setSelectedBool(e.target.value);
+}
+
+const sendDatatoApi =async()=>{
+  let  myAmount = userEnteredAmount.current.value; 
+  let myId= 1;
+  console.log("myAmount", myAmount);
+  if(myAmount>0){
+  try {
+  const response = await axios.post('https://prediction-auto-bot.herokuapp.com/api/startBatting', {
+    bol:selectedBool,
+    method: selectedMethod,
+    amount:myAmount,
+    id:myId
+  // Enter your body parameters here
+  });
+  console.log("response",response);
+  toast.success("Updated DataSuccessfully")
+}
+catch(e) {
+  // Handle your error here
+  toast.error("Error while posting data to Api")
+  console.log("Error while Posting data to api",e);
+
+}
+
+
+
+  }else{
+    toast.error("Amount must be greater than 0")
+  
+  }
+}
+
+
   return (
     <div className="container mainContainer">
-      <div className="row d-flex justify-content-center align-items-center pt-5" >
-        <div className="col-4 mt-5">
-          <Form.Select aria-label="Default select example">
-            <option value="1">Bet Bear</option>
-            <option value="2">Bet Bull</option>
-          </Form.Select>
+      <div className="row d-flex justify-content-center align-items-center">
+        <div className="col-4 ">
+          <select  onChange={(e)=>getMethod(e)} className="form-select myDropDowns" aria-label="Default select example">
+            <option value="betBear">Bet Bear</option>
+            <option value="betBull">Bet Bull</option>
+          </select>
         </div>
-        <div className="col-4 mt-5">
-          <Form.Select aria-label="Default select example">
-            <option value="1">True</option>
-            <option value="2">False</option>
-          </Form.Select>
+      
+        
+      </div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-4 mt-2 " >
+        <select onChange={(e)=>getBool(e)} className="form-select"   aria-label="Default  select example">
+            <option  value="true">True</option>
+            <option value="false">False</option>
+          </select>
         </div>
       </div>
-      <div className="row d-flex justify-content-center mt-5">
-        <div className="col-6">
-          <button className="myConfirmButton">Confirm</button>
+      <div className="row d-flex justify-content-center">
+        <div className="col-4 mt-2 light-form">
+        <div className="field">
+          <input ref={userEnteredAmount} type="text" placeholder="Amount">
+          </input>
+          <button type="submit" className="button">OK</button>
+          
+        </div>
+      
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-4 mt-2">
+          <button onClick={()=>sendDatatoApi()}  className="myConfirmButton mt-5">Confirm</button>
         </div>
       </div>
     </div>
